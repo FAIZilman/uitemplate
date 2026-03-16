25b0e38fbc13767e887f889bf032ad93d7386289
Faiz Ilman <thereisalwaysgod12@gmail.com>
2026-03-17 05:55:30 +0800

    Update Version v1.2.0

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -23,76 +23,81 @@ class InstallCommand extends Command
         if ($this->confirm("Apakah kamu yakin menginstall component {$component}?", true)) {
             $resources = realpath(__DIR__ . "/../../components/{$componentLower}/{$componentLower}.blade.php");
             $destination = base_path("resources/views/components/ui/{$componentLower}.blade.php");
-            $folder = dirname($destination);
-            if (!is_dir($folder)) {
-                mkdir($folder, 0755, true);
-            }
-            if ($view) {
-                if (!file_exists($destination)) {
-                    copy($resources, $destination);
-                    $this->line('');
-                    $this->info("UI Template Installer");
-                    $this->line('---------------------');
-                    $this->line('');
-
-                    $this->comment("Installing {$component} component...");
-                    $this->line('');
-
-                    sleep(2);
-                    $this->line("✔ {$component} component installed");
-                    sleep(1);
-                    $this->line("✔ Tailwind styles published");
-
-                    $this->line('');
-                    $this->line('Installation completed successfully!');
-                    $this->line('');
-
-                    return 0;
-                } else {
-                    $this->error("Component Blade already exists!");
-                }
+            if (!is_dir($resources) && !is_dir($destination)) {
+                $this->error("Component [{$component}] not found. Make sure the component exists.");
             } else {
-                $resourcesClass = realpath(__DIR__ . "/../../components/{$componentLower}/{$componentFirstUpper}.php");
-                $destinationClass = base_path("app/View/Components/Ui/{$componentFirstUpper}.php");
-
-                $folderClass = dirname($destinationClass);
-                if (!is_dir($folderClass)) {
-                    mkdir($folderClass, 0755, true);
+                $folder = dirname($destination);
+                if (!is_dir($folder)) {
+                    mkdir($folder, 0755, true);
                 }
-
-                if (!file_exists($destinationClass)) {
-
-                    copy($resourcesClass, $destinationClass);
-                    $this->line('');
-                    $this->info("UI Template Installer");
-                    $this->line('---------------------');
-                    $this->line('');
-
-                    $this->comment("Installing {$component} component...");
-                    $this->line('');
-
-                    sleep(2);
-                    $this->line("✔ {$component} component installed");
-                    sleep(1);
-                    $this->line("✔ Tailwind styles published");
-                    sleep(1);
-                    $this->line("✔ Config file Created");
-
+                if ($view) {
                     if (!file_exists($destination)) {
                         copy($resources, $destination);
+                        $this->line('');
+                        $this->info("UI Template Installer");
+                        $this->line('---------------------');
+                        $this->line('');
+
+                        $this->comment("Installing {$component} component...");
+                        $this->line('');
+
+                        sleep(2);
+                        $this->line("✔ {$component} component installed");
+                        sleep(1);
+                        $this->line("✔ Tailwind styles published");
+
+                        $this->line('');
+                        $this->line('Installation completed successfully!');
+                        $this->line('');
+
+                        return 0;
                     } else {
-                        $this->info("Component Blade already exists!");
+                        $this->error("Component Blade already exists!");
+                    }
+                } else {
+                    $resourcesClass = realpath(__DIR__ . "/../../components/{$componentLower}/{$componentFirstUpper}.php");
+                    $destinationClass = base_path("app/View/Components/Ui/{$componentFirstUpper}.php");
+
+                    $folderClass = dirname($destinationClass);
+                    if (!is_dir($folderClass)) {
+                        mkdir($folderClass, 0755, true);
                     }
 
-                    $this->line('');
-                    $this->line('Installation completed successfully!');
-                    $this->line('');
+                    if (!file_exists($destinationClass)) {
 
-                    return 0;
-                } else {
-                    $this->error("Component already exists!");
+                        copy($resourcesClass, $destinationClass);
+                        $this->line('');
+                        $this->info("UI Template Installer");
+                        $this->line('---------------------');
+                        $this->line('');
+
+                        $this->comment("Installing {$component} component...");
+                        $this->line('');
+
+                        sleep(2);
+                        $this->line("✔ {$component} component installed");
+                        sleep(1);
+                        $this->line("✔ Tailwind styles published");
+                        sleep(1);
+                        $this->line("✔ Config file Created");
+
+                        if (!file_exists($destination)) {
+                            copy($resources, $destination);
+                        } else {
+                            $this->info("Component Blade already exists!");
+                        }
+
+                        $this->line('');
+                        $this->line('Installation completed successfully!');
+                        $this->line('');
+
+                        return 0;
+                    } else {
+                        $this->error("Component already exists!");
+                    }
                 }
             }
+
         }
     }
 }
